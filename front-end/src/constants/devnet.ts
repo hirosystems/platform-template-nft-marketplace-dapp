e26f4abf612c583b0165cece117d27b306344ec2
beguene <beguene@gmail.com>
2024-12-10 16:41:44 +0000

    fix: update

--- front-end/src/constants/devnet.ts
+++ front-end/src/constants/devnet.ts
@@ -1,7 +1,7 @@
 import { STACKS_TESTNET, StacksNetwork } from "@stacks/network";
 
-// export const DEVNET_STACKS_BLOCKCHAIN_API_URL = `https://api.platform.hiro.so/v1/ext/${process.env.NEXT_PUBLIC_PLATFORM_HIRO_API_KEY}/stacks-blockchain-api`;
-export const DEVNET_STACKS_BLOCKCHAIN_API_URL = `http://localhost:3999`;
+export const DEVNET_STACKS_BLOCKCHAIN_API_URL = `https://api.platform.hiro.so/v1/ext/${process.env.NEXT_PUBLIC_PLATFORM_HIRO_API_KEY}/stacks-blockchain-api`;
+// export const DEVNET_STACKS_BLOCKCHAIN_API_URL = `http://localhost:3999`;
 
 export const DEVNET_NETWORK: StacksNetwork = {
   ...STACKS_TESTNET,
